10a4896a1b71c68e1dfd4d2cf8d4a365c653ae5c
schleising <stephen@schleising.net>
2021-10-09 11:28:38 +0100

    Update tk inter test

--- tk-inter-test.py
+++ tk-inter-test.py
@@ -4,9 +4,9 @@ from tkinter import filedialog, simpledialog, colorchooser
 root = tk.Tk()
 root.withdraw()
 
-# test = simpledialog.askstring('Asking for a string', 'Enter a string')
+test = simpledialog.askstring('Asking for a string', 'Enter a string')
 
-# print(test)
+print(test)
 
 filenames = filedialog.askopenfilenames()
 
@@ -14,4 +14,4 @@ print(type(filenames))
 
 for filename in filenames: print(f'Name: {filename}')
 
-colour = colorchooser.askcolor()
+# colour = colorchooser.askcolor()
