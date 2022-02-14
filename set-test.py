b1fbd1d6f02564fab635fc01e085d6d888b0229c
schleising <stephen@schleising.net>
2022-02-14 21:58:12 +0000

    Update the set test

--- set-test.py
+++ set-test.py
@@ -1,4 +1,12 @@
 set1 = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-list1 = [4, 5, 6]
+list1 = [4, 5, 6, 10]
+set2 = {5, 6, 7}
 
-print(set1 - set(list1))
+print(set1 & set(list1)) # 4, 5, 6
+print(set1 | set(list1)) # 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+print(set1 ^ set(list1)) # 1, 2, 3, 7, 8, 9, 10
+print(set1 - set(list1)) # 1, 2, 3, 7, 8, 9
+print(set1 > set2) # True (all items of set2 are in set1)
+print(set1 >= set2) # True (all items of set2 are in set1 and set2 != set1)
+print(set1 >= set1) # True (all items of set1 are in set1 and set1 == set1)
+print(set1 > set1) # False (all items of set1 are in set1 but set1 == set1)
