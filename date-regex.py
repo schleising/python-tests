b602fa21dcc73cf57df89f1f54ba750e0cc64998
schleising <stephen@schleising.net>
2022-11-15 22:53:13 +0000

    Add some error checking

--- date-regex.py
+++ date-regex.py
@@ -1,7 +1,7 @@
 from datetime import date
 import re
 
-dateString = '2022-12-20'
+dateString = '2022-11-30'
 
 dateRegex = re.compile('(202[0-9])-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])')
 
@@ -9,8 +9,12 @@ output = dateRegex.fullmatch(dateString)
 
 if output is not None:
     year, month, day = (int(group) for group in output.groups())
-    realdate = date(year, month, day)
-    print(type(realdate))
-    print(realdate)
+    try:
+        realdate = date(year, month, day)
+    except ValueError as ve:
+        print(f'Error, {ve}')
+    else:
+        print(type(realdate))
+        print(realdate)
 else:
     print('No match')
