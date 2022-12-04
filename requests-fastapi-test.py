8549f018c82d9fbb7e62a9f0d7732006f967e9d8
schleising <stephen@schleising.net>
2022-12-04 21:29:55 +0000

    Handle a ConnectionError

--- requests-fastapi-test.py
+++ requests-fastapi-test.py
@@ -3,7 +3,10 @@ import json
 import requests
 from requests.status_codes import codes
 
-response = requests.get("http://localhost:8000/items/2?q=name")
-
-if response.status_code == codes.ok:
-    print(json.dumps(json.loads(response.content), indent=2))
+try:
+    response = requests.get("http://localhost:8000/items/2?q=name")
+except requests.exceptions.ConnectionError:
+    print('Connection Refused, Exiting')
+else:
+    if response.status_code == codes.ok:
+        print(json.dumps(json.loads(response.content), indent=2))
